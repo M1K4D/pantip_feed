706469e3f98f4e0fd8cbd4c44d409c6839109ac3
rattapon simsri <rattapon.simsri0@gmail.com>
2021-02-15 14:23:48 +0700

    search in raw

--- scraper.py
+++ scraper.py
@@ -1,6 +1,6 @@
 from typing import Text
 from bs4 import BeautifulSoup
-import requests as req
+import requests
 import feedparser
 import pandas as pd
 import time
@@ -8,26 +8,38 @@ import threading as thr
 import multiprocessing as mp
 import os
 from yaspin import yaspin
+# import deepcut
 
 
-def find(arr, title):
-    for x in arr:
-        if str(x["title"]) == str(title):
-            return True
-        else:
-            return False
-
-
-def pantip_extract():
+def pantip_extract(keywords):
     feed = feedparser.parse('https://pantip.com/forum/feed')
     new_items = []
-    for item in feed.entries:
-        _item = {}
-        _item['title'] = item.title
-        _item['description'] = item.summary
-        _item['link'] = item.link
-        _item['published'] = item.published
-        new_items.append(_item)
+    if len(keywords) == 0:
+        for item in feed.entries:
+            _item = {}
+            _item['title'] = item.title
+            _item['description'] = item.summary
+            _item['link'] = item.link
+            _item['published'] = item.published
+
+            new_items.append(_item)
+    else:
+        for item in feed.entries:
+            keywords_match = False
+            _item = {}
+            _item['title'] = item.title
+            _item['description'] = item.summary
+            _item['link'] = item.link
+            _item['published'] = item.published
+
+            for keyword in keywords:
+                title = item.title
+                find = title.find(keyword)
+                if find >= 0:
+                    keywords_match = True
+
+            if keywords_match:
+                new_items.append(_item)
 
     return new_items
 
@@ -35,37 +47,36 @@ def pantip_extract():
 pantip_data = []
 
 
-def get_data(kill):
+def get_data(keywords):
     pantip_data = []
     with yaspin(text='scraping..........'):
         while True:
-            if kill():
-                break
-            data = pantip_extract()
+            data = pantip_extract(keywords)
             pantip_data.extend(data)
+
             data_clean = []
             for i in range(len(pantip_data)):
                 if pantip_data[i] not in pantip_data[i + 1:]:
                     data_clean.append(pantip_data[i])
+
             data_frame = pd.DataFrame(
                 data_clean, columns=['title', 'description', 'link', 'published'])
-            # os.system('clear')
             print("data length : ", len(data_frame))
             path = os.getcwd()
             data_frame.to_csv(path + '/export_dataframe.csv', header=True)
-            time.sleep(60)
+            time.sleep(180)
 
 
+# get_data()
 def main():
-    kill = False
-    que = mp.Queue()
-    thread = thr.Thread(target=get_data, args=[lambda:kill])
-    thread.start()
-    _input = input('want to exit ? (q)')
-    if _input.lower() == 'q':
-        kill = True
-        thread.join()
-        print('has exit')
+    search = input('input your keywords : ')
+    clean = search.split(' ')
+    print(clean)
+    if len(clean) == 0:
+        words = ''
+    else:
+        words = clean
+    get_data(words)
 
 
 main()
