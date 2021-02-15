f6bd81ad3e3cad73ce83376a9365fc72fa6bae0b
rattapon simsri <rattapon.simsri0@gmail.com>
2021-02-15 18:48:28 +0700

    update

--- choose_data.py
+++ choose_data.py
@@ -2,10 +2,11 @@ import pandas as pd
 import os
 
 path = os.getcwd()
-data_frame = pd.read_csv(path + '/export_dataframe.csv')
-data_list = data_frame.values.tolist()
-keyword = 'กินยา'
-data = []
-for data in data_list:
-    if data[1].find(keyword) != -1:
-        print(data)
+data_frame = pd.read_csv(path + '/raw_dataframe.csv')
+data_list = data_frame.to_dict('records')
+print(data_list)
+# keyword = 'เพลง'
+# data = []
+# for data in data_list:
+#     if data[1].find(keyword) != -1:
+#         print(data)
--- scraper.py
+++ scraper.py
@@ -8,41 +8,62 @@ import threading as thr
 import multiprocessing as mp
 import os
 from yaspin import yaspin
+path = os.getcwd()
 
 
-def pantip_extract():
+def pantip_extract(keyword=''):
     feed = feedparser.parse('https://pantip.com/forum/feed')
     new_items = []
-    for item in feed.entries:
-        _item = {}
-        _item['title'] = item.title
-        _item['description'] = item.summary
-        _item['link'] = item.link
-        _item['published'] = item.published
+    if len(keyword) == 0:
+        for item in feed.entries:
+            _item = {}
 
-        new_items.append(_item)
+            _item['title'] = item.title
+            _item['description'] = item.summary
+            _item['link'] = item.link
+            _item['published'] = item.published
+
+            new_items.append(_item)
+    else:
+        for item in feed.entries:
+            _item = {}
+            _item['title'] = item.title
+            _item['description'] = item.summary
+            _item['link'] = item.link
+            _item['published'] = item.published
+
+            new_items.append(_item)
+
     return new_items
 
 
-pantip_data = []
-
-
-def get_data():
-    pantip_data = []
+def get_data(keyword=''):
+    print(keyword)
+    try:
+        data_frame = pd.read_csv(path + '/raw_dataframe.csv')
+        data = data_frame.to_dict('records')
+        data_list = []
+        for list in data:
+            item = {}
+            item['title'] = list['title']
+            item['description'] = list['description']
+            item['link'] = list['link']
+            item['published'] = list['published']
+            data_list.append(item)
+    except:
+        data_list = []
     with yaspin(text='scraping..........'):
         while True:
             data = pantip_extract()
-            pantip_data.extend(data)
+            data_list.extend(data)
 
-            remove_dup = []
-            for i in range(len(pantip_data)):
-                if pantip_data[i] not in pantip_data[i + 1:]:
-                    remove_dup.append(pantip_data[i])
+            remove_dup = [i for n, i in enumerate(
+                data_list) if i not in data_list[n + 1:]]
 
             data_frame = pd.DataFrame(
                 remove_dup, columns=['title', 'description', 'link', 'published'])
-            print("data length : ", len(data_frame))
-            path = os.getcwd()
+            print("data length : ", len(remove_dup))
+
             data_frame.to_csv(path + '/raw_dataframe.csv', header=True)
             time.sleep(60)
 
