7a1599b81e51ff56cd16fb81328b3897651cd7e5
rattapon simsri <rattapon.simsri0@gmail.com>
2021-02-16 17:37:22 +0700

    update

--- choose_data.py
+++ choose_data.py
@@ -1,12 +1,33 @@
 import pandas as pd
 import os
+import requests
+import pandas as pd
+path = os.getcwd()
 
+url = "https://api.aiforthai.in.th/ssense"
 path = os.getcwd()
 data_frame = pd.read_csv(path + '/raw_dataframe.csv')
 data_list = data_frame.to_dict('records')
-print(data_list)
-# keyword = 'เพลง'
-# data = []
-# for data in data_list:
-#     if data[1].find(keyword) != -1:
-#         print(data)
+
+choose = []
+for data in data_list:
+    text = str(data['description'])
+    if text.find('หุ้น') != -1:
+        choose.append(data)
+results = []
+for sen_ in choose:
+    result = {}
+    text = sen_['description']
+    data = {'text': text}
+    headers = {
+        'Apikey': "NHSK9EnQDZNi5Rb24YKESf2BknHK6Ni0"
+    }
+    response = requests.post(url, data=data, headers=headers)
+    sentiment = response.json()
+    result['text'] = text
+    result['sentiment'] = sentiment['sentiment']['polarity']
+    results.append(result)
+
+data_frame = pd.DataFrame(results, columns=['text', 'sentiment'])
+data_frame.to_csv(path + '/sentimental.csv', header=True)
+print(data_frame)
--- scraper.py
+++ scraper.py
@@ -8,10 +8,12 @@ import threading as thr
 import multiprocessing as mp
 import os
 from yaspin import yaspin
+import ast
 path = os.getcwd()
 
 
 def pantip_extract(keywords=''):
+    print(keywords)
     feed = feedparser.parse('https://pantip.com/forum/feed')
     new_items = []
     if len(keywords) == 0:
@@ -34,23 +36,24 @@ def pantip_extract(keywords=''):
             _item['published'] = item.published
 
             for keyword in keywords:
-                title = item.title
+                title = str(item.title)
+                description = str(item.summary)
                 find = title.find(keyword)
-                if find >= 0:
+                des = description.find(keyword)
+
+                if find >= 0 or des >= 0:
                     keywords_match = True
 
             if keywords_match:
                 new_items.append(_item)
 
-            new_items.append(_item)
-
     return new_items
 
 
 def get_data(keywords=''):
-    # print(keywords)
     try:
-        data_frame = pd.read_csv(path + '/raw_dataframe.csv')
+        data_frame = pd.read_csv(
+            '/home/m1k4/project/nodejs_search/raw_dataframe2.csv')
         data = data_frame.to_dict('records')
         data_list = []
         for list in data:
@@ -67,15 +70,20 @@ def get_data(keywords=''):
             data = pantip_extract(keywords)
             data_list.extend(data)
 
-            remove_dup = [i for n, i in enumerate(
-                data_list) if i not in data_list[n + 1:]]
+            remove_dup = []
+            for i in range(len(data_list)):
+                if data_list[i] not in data_list[i + 1:]:
+                    remove_dup.append(data_list[i])
 
             data_frame = pd.DataFrame(
                 remove_dup, columns=['title', 'description', 'link', 'published'])
             print("data length : ", len(remove_dup))
 
-            data_frame.to_csv(path + '/raw_dataframe.csv', header=True)
+            data_frame.to_csv(
+                '/home/m1k4/project/nodejs_search/raw_dataframe2.csv', header=True)
+            data_frame.to_csv(
+                path + '/raw_dataframe2.csv', header=True)
             time.sleep(60)
 
 
-get_data(['อนิเมะ'])
+get_data(['การ์ตูน', 'อนิเมะ', 'เพลง', 'หุ้น'])
